25439fe16bf230addbb6a4808e16777037c3a98c
twentytw <halbatech32@gmail.com>
2025-11-27 19:43:27 +0100

    Delete plugin.program.kronos.shield directory

--- plugin.program.kronos.shield/lib/vpn_utils.py
+++
@@ -1,9 +0,0 @@
-import os
-import subprocess
-
-def is_vpn_active():
-    try:
-        result = subprocess.check_output(['ip', 'addr'], text=True)
-        return 'tun0' in result
-    except Exception:
-        return False
